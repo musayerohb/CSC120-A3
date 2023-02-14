1925985c7d15d7a71bb6b752e8b86ed6b662a926
Musayeroh Bah <mbah@smith.edu>
2023-02-13 20:03:46 -0500

    2nd day

--- Conversation.java
+++ Conversation.java
@@ -7,25 +7,61 @@ class Conversation {
   public static void main(String[] arguments) {
 
     String[] canned_responses = {"That's so cool!", "Wow!", "I never thought about that.", "Oh...", "Interesting..."};
-    String[] mirror_words = {"I", "me", "am", "you", "my", "your", "are"};
 
     // You will start the conversation here.
     Scanner userInput = new Scanner(System.in);
     Random random = new Random();
+
     System.out.println("How many rounds? ");
     int round_number = userInput.nextInt();
+    userInput.nextLine();
 
     int rounds_passed = 0;
     System.out.println("Hi there! What do you want to talk about?");
-
+    boolean should_response_change = false;
+    
     while (rounds_passed != round_number) {
-      String user_response = userInput.next();
-      int index = random.nextInt(canned_responses.length);
-      System.out.println(canned_responses[index]);
-      rounds_passed = rounds_passed + 1;
+      String user_response = userInput.nextLine();
       
+      // Look for words to mirror
+      String[] user_response_list = user_response.split(" ");
+
+      for (int i = 0; i < user_response_list.length; i++){
+        if (user_response_list[i].equals("I")){
+          int position = i;
+          user_response_list[position] = ("you");
+          should_response_change = true;
+          continue;
+        }
+        else if (user_response_list[i].equals("you")){
+          int position = i;
+          user_response_list[position] = ("I");
+          should_response_change = true;
+          continue;
+        }
+        else if (user_response_list[i].equals("your")){
+          int position = i;
+          user_response_list[position] = ("my");
+          should_response_change = true;
+          continue;
+        }
+        else if (user_response_list[i].equals("are")){
+          int position = i;
+          user_response_list[position] = ("am");
+          should_response_change = true;
+          continue;
+        }
+      }
+      
+      if (should_response_change == true) {
+        String mirror_response = String.join(" ", user_response_list);
+        System.out.println(mirror_response + "?");
+      }
+      else {
+        int index = random.nextInt(canned_responses.length);
+        System.out.println(canned_responses[index]);
+      }
+      rounds_passed = rounds_passed + 1;
     }
-    
-    
   }
 }
--- reflection.md
+++ reflection.md
@@ -6,4 +6,8 @@ Use this file to record your reflection on this assignment.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
 
-Eva and I consulted each other first on this assignment and worked together on it on 2/11. 
+Eva and I consulted each other first on this assignment and worked together on it on 2/11. Some parts of Java is similar to Python, but the way we use classes is really different because basically no code can exist without referencing classes. We knew what we had to implement based on what we learned in Python, but implementing it with the right syntax was pretty hard. 
+
+Both Eva and I attended TA hours on Sunday 2/12 at 1pm.
+I attended TA hours on Monday 2/13 - fixed while loop glitch by using .equals and 
+
