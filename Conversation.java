84522528671ffa8cb0d140e21e3237a7756cfc9a
Musayeroh Bah <mbah@smith.edu>
2023-02-15 23:38:03 -0500

    submit

--- Conversation.java
+++ Conversation.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 import java.util.Random;
-
+import java.util.ArrayList;
 
 class Conversation {
-
   public static void main(String[] arguments) {
 
     String[] canned_responses = {"That's so cool!", "Wow!", "I never thought about that.", "Oh...", "Interesting..."};
+
     
     // You will start the conversation here.
     Scanner userInput = new Scanner(System.in);
@@ -18,23 +18,22 @@ class Conversation {
 
     int round_number = userInput.nextInt();
     userInput.nextLine();
-    String[] transcript = new String[round_number*2 + 2];
+    ArrayList <String> transcript = new ArrayList<String>();
 
     int rounds_passed = 0;
     System.out.println("Hi there! What do you want to talk about?");
-    transcript[0] = "Hi there! What do you want to talk about?";
-    int transcript_index = 0;
+    transcript.add("Hi there! What do you want to talk about?");
 
     boolean should_response_change = false;
-    
+
     while (rounds_passed != round_number) {
       String user_response = userInput.nextLine();
-      transcript[(transcript_index+1)] = user_response;
-      
+      transcript.add(user_response);
+
       // Look for words to mirror
       String[] user_response_list = user_response.split(" ");
       should_response_change = false;
-      
+
       for (int i = 0; i < user_response_list.length; i++){
         if (user_response_list[i].equals("I")){
           int position = i;
@@ -48,9 +47,33 @@ class Conversation {
           should_response_change = true;
           continue;
         }
+        else if (user_response_list[i].equals("You")){
+          int position = i;
+          user_response_list[position] = ("I");
+          should_response_change = true;
+          continue;
+        }
         else if (user_response_list[i].equals("your")){
           int position = i;
           user_response_list[position] = ("my");
+          should_response_change = true;
+          continue;
+        }
+        else if (user_response_list[i].equals("Your")){
+          int position = i;
+          user_response_list[position] = ("My");
+          should_response_change = true;
+          continue;
+        }
+        else if (user_response_list[i].equals("my")){
+          int position = i;
+          user_response_list[position] = ("your");
+          should_response_change = true;
+          continue;
+        }
+        else if (user_response_list[i].equals("My")){
+          int position = i;
+          user_response_list[position] = ("Your");
           should_response_change = true;
           continue;
         }
@@ -60,27 +83,34 @@ class Conversation {
           should_response_change = true;
           continue;
         }
+        else if (user_response_list[i].equals("Are")){
+          int position = i;
+          user_response_list[position] = ("Am");
+          should_response_change = true;
+          continue;
+        }
       }
-      
-      if (should_response_change == true) {
-        String mirror_response = String.join(" ", user_response_list);
-        System.out.println(mirror_response + "?");
-        transcript[(transcript_index+1)] = mirror_response + "?";
-      }
-      else {
-        int index = random.nextInt(canned_responses.length);
-        String random_canned_response = canned_responses[index]; 
-        System.out.println(random_canned_response);
-        transcript[(transcript_index+1)] = random_canned_response;
-      }
-      rounds_passed = rounds_passed + 1;
-    }
-    userInput.close();
-  
-    System.out.println("Transcript");
-    for (int m = 0; m < transcript.length; m++) {
-      System.out.println(transcript[m]);
+    if (should_response_change == true) {
+      String mirror_response = String.join(" ", user_response_list);
+      System.out.println(mirror_response + "?");
+      transcript.add(mirror_response + "?");
     }
 
+    else {
+      int index = random.nextInt(canned_responses.length);
+      //String random_canned_response = canned_responses[index]; 
+      System.out.println(canned_responses[index]);
+      transcript.add(canned_responses[index]);
+    }
+    
+    rounds_passed = rounds_passed + 1;
+  }
+    System.out.println("See ya!");
+    transcript.add("See ya!");
+    System.out.println("");
+    System.out.println("Transcript:");
+    System.out.println(transcript);
+    userInput.close();
+    
   }
 }
